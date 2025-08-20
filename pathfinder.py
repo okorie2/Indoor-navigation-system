b9b95c166e088246a564e33d2ff58299ea0ccf74
okorie2 <ellahokorie@gmail.com>
2025-08-20 01:42:25 +0100

    feat: add direction and distance to edges

--- pathfinder.py
+++ pathfinder.py
@@ -29,33 +29,32 @@ def a_star(graph, start, goal):
     f_score[start] = heuristic(start, goal)  # why?
 
     while open_set:
-        _, current = heapq.heappop(open_set)
+        _, current_node = heapq.heappop(open_set)
 
-        if current == goal:
+        if current_node == goal:
             # reconstruct path
-            path = [current]
-            while current in came_from:
-                current = came_from[current]
-                path.append(current)
+            path = [current_node]
+            while current_node in came_from:
+                current_node = came_from[current_node]
+                path.append(current_node)
             return path[::-1]
 
         # considering the edges of the current node
-        for neighbor_info in graph.get(current, []):
-            neighbor = neighbor_info["to"]
-            weight = neighbor_info["weight"]
+        for edge_details in graph.get(current_node, []):
+            edge_name = edge_details["to"]
+            edge_weight = edge_details["weight"]
             # on first iteration, g_score[current] is 0, so tentative_g is just the weight of the edge
-            tentative_g = g_score[current] + weight
+            tentative_g = g_score[current_node] + edge_weight
             # comparing tentative_g with the current g_score of the neighbor. on first iteration, g_score[neighbor] is inf
-            if tentative_g < g_score.get(neighbor, float("inf")):
+            if tentative_g < g_score.get(edge_name, float("inf")):
                 # print(current, "current")
-                came_from[neighbor] = current
-                g_score[neighbor] = tentative_g
-                f_score[neighbor] = tentative_g + heuristic(neighbor, goal)
-                heapq.heappush(open_set, (f_score[neighbor], neighbor))
+                came_from[edge_name] = current_node
+                g_score[edge_name] = tentative_g
+                f_score[edge_name] = tentative_g + heuristic(edge_name, goal)
+                heapq.heappush(open_set, (f_score[edge_name], edge_name))
                 # print(came_from, "came_from")
 
     return None
 
 
-# Example run
-print(a_star(graph, "cafeBar", "southEntrance"))
+print(a_star(graph, "SN211", "SN203"))
